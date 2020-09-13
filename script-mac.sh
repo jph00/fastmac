aa89309820c7ce7735129664b8e15c77322a275a
Jeremy Howard <j@fast.ai>
2020-09-13 06:47:59 -0700

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -1,3 +1,9 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 conda init bash
+echo "conda activate" >> ~/.bash_profile
+sudo conda config --add channels fastai
+
+git config --global pull.rebase true
+git config --global user.name "Jeremy Howard"
+git config --global user.email j@fast.ai
