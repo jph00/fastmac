4995d8d0925b7f5a5e1151ec0028a9bca6595b27
Jeremy Howard <j@fast.ai>
2020-09-13 05:17:35 -0700

    Update setup-conda.sh

--- setup-conda.sh
+++ setup-conda.sh
@@ -4,13 +4,13 @@ set -e
 cd
 
 case "$OSTYPE" in
-  darwin*)  DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh ;;
-  linux*)   DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh  ;;
+  darwin*)  DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh; RC_FILE=.bash_profile ;;
+  linux*)   DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh; RC_FILE=.bashrc  ;;
   *)        echo "unknown: $OSTYPE" ;;
 esac
 
 case "$SHELL" in
-  /bin/zsh*)   SHELL_NAME=zsh  ;;
+  /bin/zsh*)   SHELL_NAME=zsh ;;
   /bin/bash*)  SHELL_NAME=bash ;;
   *)        echo "unknown: $SHELL" ;;
 esac
@@ -28,9 +28,9 @@ bash Miniconda3-latest*.sh -b
 ~/miniconda3/bin/conda init $SHELL_NAME
 rm Miniconda3-latest*.sh
 
-perl -n  -e 'print if     />>> conda/../<<< conda/' .bashrc > .condainit
-perl -ni -e 'print unless />>> conda/../<<< conda/' .bashrc
-echo source ~/.condainit >> .bashrc
+perl -n  -e 'print if     />>> conda/../<<< conda/' $RCFILE > .condainit
+perl -ni -e 'print unless />>> conda/../<<< conda/' $RCFILE
+echo source ~/.condainit >> $RCFILE
 source .condainit
 
 conda install -y mamba
