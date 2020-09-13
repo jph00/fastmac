d3a7cb9cbde2c42c6d95166e61143db0acdcc44c
Jeremy Howard <j@fast.ai>
2020-09-13 06:48:48 -0700

    Update setup-conda.sh

--- setup-conda.sh
+++ setup-conda.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-. ~/.bash_profile
-conda activate
-sudo conda config --add channels fastai
 sudo conda install -yc defaults -c conda-forge mamba
-sudo mamba install -y conda-build anaconda-client
+sudo mamba install -y conda-build anaconda-client conda-verify
